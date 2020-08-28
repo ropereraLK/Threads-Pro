434a0eec5186ef4a6e3df785cb5bd975772e4343
Rohan Perera <ropereralk@gmail.com>
2020-08-28 22:32:57 +0530

    initial commit

---
+++ src/main/java/com/threadspro/ThreadsProApplication.java
@@ -0,0 +1,13 @@
+package com.threadspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadsProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadsProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/threadspro/ThreadsProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threadspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadsProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
